105781a39b863e82f1ad5e326d39c5d87e2f85d4
Pancho Labrador <plabradorjr@gmail.com>
2023-04-30 15:23:40 -0500

    schema updated for shipping info

---
+++ prisma/migrations/20230430202010_shipping_address_form/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "address" TEXT,
+ADD COLUMN     "aptSuiteEtc" TEXT,
+ADD COLUMN     "city" TEXT,
+ADD COLUMN     "country" TEXT,
+ADD COLUMN     "firstName" TEXT,
+ADD COLUMN     "lastName" TEXT,
+ADD COLUMN     "state" TEXT,
+ADD COLUMN     "zipCode" TEXT;
