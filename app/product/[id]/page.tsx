b926cdd9308c483c8850316b5a59e1fe3172b406
Pancho Labrador <plabradorjr@gmail.com>
2023-05-01 19:53:17 -0500

    conditionally renders button sizes

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -4,8 +4,14 @@ import formatPrice from "@/util/PriceFormat";
 import AddCart from "./AddCart";
 
 export default async function Product({ searchParams }: SearchParamTypes) {
+  const currentSize = searchParams.size;
+
+  const getClassName = (size: string) => {
+    return currentSize === size ? "btn btn-primary" : "btn btn-outline";
+  };
+
   return (
-    <div className="flex flex-col 2xl:flex-row items-center justify-between gap-16 ">
+    <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-between px-5 gap-5">
       <Image
         src={searchParams.image}
         alt={searchParams.name}
@@ -18,6 +24,13 @@ export default async function Product({ searchParams }: SearchParamTypes) {
       <div className="font-medium ">
         <h1 className="text-2xl  py-2">{searchParams.name}</h1>
         <p className="py-2">{searchParams.description}</p>
+        <div className="grid grid-cols-4 gap-2">
+          <button className={getClassName("small")}>Small</button>
+          <button className={getClassName("medium")}>Medium</button>
+          <button className={getClassName("large")}>Large</button>
+          <button className={getClassName("x-large")}>X-Large</button>
+        </div>
+
         <p className="py-2">{searchParams.size}</p>
         <div className="flex gap-2">
           <p className="font-bold text-primary">
