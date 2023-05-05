e670e03ba1fbbade274882af91bf1f7722b0dbda
Pancho Labrador <plabradorjr@gmail.com>
2023-05-04 22:59:56 -0500

    privacy policy added

---
+++ app/privacy/page.tsx
@@ -0,0 +1,145 @@
+export default function Terms() {
+  return (
+    <div className="p-5">
+      <h1 className="text-3xl pb-5 justify-center text-center">
+        Privacy Policy
+      </h1>
+      <h1>Privacy Policy for shill.lol</h1>
+      <p className="pb-5">
+        This Privacy Policy describes how your personal information is
+        collected, used, and shared when you visit or make a purchase from
+        shill.lol (the “Site”).
+      </p>
+      <h1>PERSONAL INFORMATION WE COLLECT</h1>
+      <p className="pb-5">
+        When you visit the site, we automatically collect certain information
+        about your device, including information about your web browser, IP
+        address, time zone, and some of the cookies that are installed on your
+        device. Additionally, as you browse the Site, we collect information
+        about the individual web pages or products that you view, what websites
+        or search terms referred you to the Site, and information about how you
+        interact with the Site. We refer to this automatically-collected
+        information as “Device Information.” We collect Device Information using
+        the following technologies: “Cookies” are data files that are placed on
+        your device or computer and often include an anonymous unique
+        identifier. For more information about cookies, and how to disable
+        cookies, visit http://www.allaboutcookies.org . “Log files” track
+        actions occurring on the Site, and collect data including your IP
+        address, browser type, Internet service provider, referring/exit pages,
+        and date/time stamps. “Web beacons,” “tags,” and “pixels” are electronic
+        files used to record information about how you browse the Site.
+        Additionally, when you make a purchase or attempt to make a purchase
+        through the Site, we collect certain information from you, including
+        your name, billing address, shipping address, payment information
+        (including credit card numbers, email address, Metamask, Eth address and
+        phone number.) We refer to this information as “Order Information.” When
+        we talk about “Personal Information” in this Privacy Policy, we are
+        talking both about Device Information and Order Information.
+      </p>
+      <h1>HOW DO WE USE YOUR PERSONAL INFORMATION?</h1>
+      <p className="pb-5">
+        We use the Order Information that we collect generally to fulfill any
+        orders placed through the Site (including processing your payment
+        information, arranging for shipping, and providing you with invoices
+        and/or order confirmations). Additionally, we use this Order Information
+        to: Communicate with you; Screen our orders for potential risk or fraud;
+        and When in line with the preferences you have shared with us, provide
+        you with information or advertising relating to our products or
+        services. We use the Device Information that we collect to help us
+        screen for potential risk and fraud (in particular, your IP address),
+        and more generally to improve and optimize our Site (for example, by
+        generating analytics about how our customers browse and interact with
+        the Site, and to assess the success of our marketing and advertising
+        campaigns).
+      </p>
+      <h1>SHARING YOUR PERSONAL INFORMATION</h1>
+      <p className="pb-5">
+        We share your Personal Information with third parties to help us use
+        your Personal Information, as described above. For example, we use
+        Google Analytics to help us understand how our customers use the
+        Site--you can read more about how Google uses your Personal Information
+        here: https://www.google.com/intl/en/policies/privacy/ . We use Stripe
+        and Stripe Payments to help us process your Order Information, including
+        to securely take your payment information and facilitate receipt of
+        payment for anything you order from the Site, as well as handle
+        shipping, and any returns, refunds and exchanges that you may request.
+        We are not responsible for Stripe’s security and data privacy efforts
+        generally, nor their specific efforts with regard to keeping your data
+        secure and the handling of your data, though they represent that they
+        are PCI compliant. We may transfer or assign this Privacy Policy and any
+        Personal Information to a third party entity in connection with a
+        merger, acquisition, consolidation, restructuring, the sale of
+        substantially all of our shares and/or assets, in connection with
+        bankruptcy proceedings, reorganization, or other change of control
+        events. Finally, we may also share your Personal Information if needed
+        to comply with applicable laws and regulations, to respond to a
+        subpoena, search warrant or other lawful request for information we
+        receive, or to otherwise protect our rights.
+      </p>
+      <h1>BEHAVIORAL ADVERTISING A</h1>
+      <p className="pb-5">
+        As described above, we use your Personal Information to provide you with
+        targeted advertisements or marketing communications we believe may be of
+        interest to you. For more information about how targeted advertising
+        works, you can visit the Network Advertising Initiative’s (“NAI”)
+        educational page at
+        http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work
+        . You can opt out of certain targeted advertising by following the
+        instructions at these sites: FACEBOOK -
+        https://www.facebook.com/settings/?tab=ads GOOGLE -
+        https://www.google.com/settings/ads/anonymous Additionally, you can opt
+        out of some of these services by visiting the Digital Advertising
+        Alliance’s opt-out portal at: http://optout.aboutads.info/.
+      </p>
+      <h1>DO NOT TRACK</h1>
+      <p className="pb-5">
+        Please note that we do not alter our Site’s data collection and use
+        practices when we see a Do Not Track signal from your browser.
+      </p>
+      <h1>YOUR RIGHTS</h1>
+      <p className="pb-5">
+        If you are a European resident, please know that the Site and everything
+        offered for sale through the Site is offered on a limited basis, and is
+        not available worldwide. Please refer to our Policy for Shipping,
+        Returns, and Refunds, which applies and specifies that we only ship
+        domestically, to US territories and military addresses. Still, if you
+        are a European resident you have the right to access personal
+        information we hold about you and to ask that your personal information
+        be corrected, updated, or deleted. If you would like to exercise this
+        right, please contact us through the contact information provided below.
+        Additionally, if you are a European resident we note that we are
+        processing your information in order to fulfill contracts we might have
+        with you, or otherwise to pursue our legitimate business interests
+        listed above. Additionally, please note that your information will be
+        transferred outside of Europe, including to Canada and the United
+        States.
+      </p>
+      <h1>DATA RETENTION</h1>
+      <p className="pb-5">
+        When you place an order through the Site, we will maintain your Order
+        Information for our records unless and until you ask us to delete this
+        information.
+      </p>
+      <h1>MINORS</h1>
+      <p className="pb-5">
+        The Site is not intended for individuals under the age of 18. We do not
+        sell products or services to children, but we do sell them to adults,
+        who can purchase with a credit card or other permitted payment method.
+        If you are under 18, you may use the Shill.lol Services only with
+        involvement and permission of a parent or guardian.
+      </p>
+      <h1>CHANGES</h1>
+      <p className="pb-5">
+        We may update this Privacy Policy from time to time in order to reflect,
+        for example, changes to our practices or for other operational, legal or
+        regulatory reasons.
+      </p>
+      <h1>CONTACT US</h1>
+      <p className="pb-5">
+        For more information about our privacy practices, if you have questions,
+        or if you would like to make a complaint, please contact us by e-mail at
+        legal@shill.lol.
+      </p>
+    </div>
+  );
+}
