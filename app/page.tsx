f34779e438b7d80cad507e8f20ceedf9d17cf1e7
Pancho Labrador <plabradorjr@gmail.com>
2023-05-04 23:10:20 -0500

    degen

--- app/page.tsx
+++ app/page.tsx
@@ -19,10 +19,10 @@ export default function Home() {
         <div className="mx-auto max-w-2xl py-24 sm:py-48 lg:py-24">
           <div className="text-center">
             <h1 className="lg:text-6xl font-bold tracking-tight text-white text-5xl">
-              KEK Swegs
+              Degen Swegs
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-300">
-              we gonna make it?
+              you gonna make it?
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
