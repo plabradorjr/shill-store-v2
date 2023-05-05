64b1133de28d5888dd03c325e58991c48c35394f
Pancho Labrador <plabradorjr@gmail.com>
2023-05-04 22:47:50 -0500

    terms page added

---
+++ app/terms/page.tsx
@@ -0,0 +1,251 @@
+export default function Terms() {
+  return (
+    <div className="p-5">
+      <h1 className="text-3xl pb-5 justify-center text-center">
+        Terms of service
+      </h1>
+      <h2>TERMS OF SERVICE AND CONDITIONS OF USE FOR SHILL.LOL STORE</h2>
+      <p className="pb-5">
+        Welcome! Shill.lol at shill.lol provides website features, products and
+        services to you when you visit or shop at shill.lol, or use other
+        Shill.lol products, services, websites or mobile applications
+        (collectively with shill.lol, "Shill.lol Services") – all of which are
+        subject to these Terms of Service and Conditions of Use. LSTLLL Holdings
+        Inc. dba Shill.lol (“Shill.lol”) reserves the right to modify these
+        Terms of Service and Conditions of Use at any time and without notice to
+        you. By visiting shill.lol or using other Shill.lol Services, you agree
+        to these Terms of Service and Conditions of Use. Please read them
+        carefully.
+      </p>
+
+      <h2>LICENSE AND ACCESS</h2>
+
+      <p className="pb-5">
+        Subject to your full compliance with these Terms of Service and
+        Conditions of Use, and your payment of any applicable fees, grants you a
+        limited, non-exclusive, non-transferable, non-sublicensable and fully
+        revocable license to access and make personal, non-commercial use of the
+        Shill.lol Services. For shill.lol specifically, the license to access is
+        solely for the purpose of shopping for items for personal use sold on
+        shill.lol and not for any commercial use or use on behalf of any third
+        party, except as explicitly permitted by us in advance and in our sole
+        discretion. The license granted herein does not include: any resale or
+        commercial use of any Shill.lol Service, or its contents; any collection
+        or use of any product listings, descriptions, images, or prices; any
+        derivative use of any Shill.lol Service or its contents; any
+        downloading, copying, or other use of account information for the
+        benefit of any third party; or any use of data mining, robots (bots), or
+        similar data gathering and extraction tools. All rights not expressly
+        granted to you in these Terms of Service and Conditions of Use are
+        reserved and retained by Shill.lol or its licensors, suppliers,
+        publishers, rightsholders, or other content providers. No Shill.lol
+        Service, nor any part of any Shill.lol Service, may be reproduced,
+        duplicated, copied, sold, resold, visited, or otherwise exploited for
+        any commercial purpose without our express written consent. You may not
+        frame or utilize framing techniques to enclose any trademark, logo, or
+        other proprietary information (including images, text, page layout, or
+        form) of Shill.lol without express written consent. You may not use any
+        meta tags or any other "hidden text" utilizing Shill.lol name or
+        trademarks without our express written consent. We reserve the right to
+        refuse service, terminate accounts, and/or cancel orders in our sole
+        discretion, including, without limitation, if we believe that customer
+        conduct violates these Terms of Service and Conditions of Use,
+        applicable law, or is harmful to our interests. In other words, you may
+        not misuse Shill.lol Services.
+      </p>
+
+      <h2>INTERNATIONAL ACCESS; SANCTIONS & EXPORT POLICY</h2>
+      <p className="pb-5">
+        Shill.lol Services may be accessible from countries other than the
+        United States and its territories and may contain products or references
+        to products or services that are not available outside of the United
+        States or its territories. Any such access or reference does not imply
+        that such products or services will be made available outside the United
+        States or its territories. If you access and use Shill.lol Services
+        outside the United States or its territories, you are responsible for
+        complying with all applicable laws and regulations. You may not use any
+        Shill.lol Services if you are the subject of U.S. sanctions or of
+        sanctions consistent with U.S. law imposed by the governments of the
+        country where you are using Shill.lol Services. You must comply with all
+        U.S. or other export and re-export restrictions that may apply to goods,
+        software, technology, and services.
+      </p>
+
+      <h2>YOUR ACCOUNT</h2>
+      <p className="pb-5">
+        You may need your own account to use certain Shill.lol Services, and you
+        may be required to be logged in to the account. You are solely
+        responsible for maintaining the confidentiality of your account and
+        password and for restricting access to your account, and you agree to
+        accept responsibility for all activities that occur under your account
+        or password. You shall immediately notify us of any actual or suspected
+        unauthorized use of your password or account. We have no obligation to
+        investigate the authorization or source of any such access or use of
+        Shill.lol Services. We do not sell products or services to children, but
+        we do sell them to adults, who can purchase with a credit card or other
+        permitted payment method. If you are under 18, you may use the Shill.lol
+        Services only with involvement and permission of a parent or guardian.
+        We reserve the right to refuse service, terminate accounts, terminate
+        your rights to use Shill.lol Services, or cancel orders in our sole
+        discretion.
+      </p>
+
+      <h2>ELECTRONIC COMMUNICATIONS</h2>
+      <p className="pb-5">
+        When you use Shill.lol Services, or send e-mails, text messages, and
+        other communications from your desktop or mobile device to us, you may
+        be communicating with us electronically. You consent to receive
+        communications (including marketing communications, unless you have
+        opted-out where applicable) from us electronically, such as e-mails,
+        texts, mobile push notices, or notices and messages on this site or
+        through the other Shill.lol Services. You agree that all notices,
+        disclosures, agreements, and other communications that we provide to you
+        electronically satisfy any legal requirement that such communication be
+        in writing.
+      </p>
+
+      <h2>COPYRIGHT & TRADEMARKS</h2>
+
+      <p className="pb-5">
+        All content included in or made available through any Shill.lol Service,
+        such as text, graphics, logos, button icons, images, audio, video, and
+        software is the property of Shill.lol; and the compilation of all
+        content included in or made available through any Shill.lol Service is
+        the exclusive property of Shill.lol and is protected by United States
+        and international copyright laws, as applicable. In addition, graphics,
+        logos, page headers, button icons, scripts, and service names included
+        in or made available through any Shill.lol Service are trademarks or
+        trade dress of Shill.lol in the U.S. and other countries. The use of any
+        of our intellectual property without our express written consent is
+        strictly prohibited. Shill.lol’ trademarks, trade dress and other
+        intellectual property may not be used in connection with any product or
+        service that is not Shill.lol’, in any manner that is likely to cause
+        confusion among customers, or in any manner that disparages or
+        discredits Shill.lol. All other trademarks not owned by Shill.lol that
+        appear in any Shill.lol Service are the property of their respective
+        owners, who may or may not be affiliated with, connected to, or
+        sponsored by us. You may not use any of our trademarks or service marks
+        in meta tags without our prior explicit consent. PRIVACY Please review
+        our Privacy Policy, which explains our practices and also governs your
+        use of Shill.lol Services. Our Privacy Policy is available here. RISK OF
+        LOSS, RETURNS/REFUNDS & TITLE The risk of loss and title for items
+        purchased by you from Shill.lol pass to you upon our delivery of the
+        items to the carrier. Conversely, we do not take title to returned items
+        until the item is received at our fulfillment center. At our discretion,
+        a refund may be issued without requiring a return. In this situation, we
+        do not take title to the refunded item. You are responsible for
+        reviewing the contents of your returns prior to shipment back to us. We
+        are not responsible for any additional items that might accompany your
+        returned items. Any such items are subject to destruction, and we have
+        no obligation to return them to you. PRODUCT DESCRIPTIONS; TYPOGRAPHICAL
+        ERRORS; SITE CONTENT Shill.lol attempts to be as accurate as possible.
+        However, Shill.lol does not warrant that product descriptions or other
+        content of any Shill.lol Service is 100% accurate, complete, reliable,
+        current, or error-free. If a product offered is not as described, your
+        sole remedy against us is to return it in unused condition and request a
+        refund for such item. If any other Shill.lol Service is not as
+        described, your sole remedy against us is to request a refund for such
+        Shill.lol Service. In the event a product or service is listed at an
+        incorrect price or with incorrect information (due to typographical
+        error or error in pricing or product information received from our
+        suppliers), we shall have the right to refuse or cancel any orders
+        placed for such product or service. We shall have the right to refuse or
+        cancel any such orders whether or not the order has been confirmed and
+        your credit card charged. If your credit card has already been charged
+        for the purchase and your order is canceled, we will issue a credit to
+        your credit card account in the amount of the charge. Content provided
+        on shill.lol is solely for informational purposes. It is your sole
+        responsibility to consult a licensed physician or qualified health care
+        professional for advice, diagnosis, and/or treatment of any health
+        related condition or before engaging in any physical activity using our
+        products or services, if appropriate.
+      </p>
+
+      <h2>TERMINATION AND EFFECT OF TERMINATION</h2>
+
+      <p className="pb-5">
+        In addition to any other legal or equitable remedies, we may, without
+        prior notice to you, immediately terminate or revoke any or all of your
+        permissions granted under these Terms of Service and Conditions of Use.
+        Upon such termination or revocation, you shall immediately cease all
+        access to and use of all Shill.lol Services, and we shall, in addition
+        to any other legal or equitable remedies, immediately revoke all
+        password(s) and account identification issued to you and deny your
+        access to and use our website in whole or in part. Any termination of
+        these Terms of Service and Conditions of Use shall not affect the
+        respective rights and obligations (including without limitation, payment
+        obligations) of the parties arising before the date of termination.
+        DISCLAIMER AND LIMITATION OF LIABILITY Shill.lol SERVICES AND ALL
+        INFORMATION, CONTENT, MATERIALS, PRODUCTS (INCLUDING SOFTWARE) AND OTHER
+        SERVICES INCLUDED ON OR OTHERWISE MADE AVAILABLE TO YOU THROUGH
+        Shill.lol SERVICES ARE PROVIDED ON AN "AS IS" AND "AS AVAILABLE" BASIS,
+        UNLESS OTHERWISE SPECIFIED IN WRITING. Shill.lol MAKES NO
+        REPRESENTATIONS OR WARRANTIES OF ANY KIND, EXPRESS OR IMPLIED, AS TO THE
+        OPERATION OF Shill.lol SERVICES, OR THE INFORMATION, CONTENT, MATERIALS,
+        PRODUCTS (INCLUDING SOFTWARE) OR OTHER SERVICES INCLUDED ON OR OTHERWISE
+        MADE AVAILABLE TO YOU THROUGH Shill.lol SERVICES, UNLESS OTHERWISE
+        SPECIFIED IN WRITING. YOU EXPRESSLY AGREE THAT YOUR USE OF Shill.lol
+        SERVICES IS AT YOUR SOLE RISK. TO THE FULL EXTENT PERMISSIBLE BY LAW,
+        Shill.lol DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING, BUT
+        NOT LIMITED TO, DATA ACCURACY, DATA SECURITY, AND IMPLIED WARRANTIES OF
+        MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. shop.lol MAY
+        INCLUDE INACCURACIES, MISTAKES OR TYPOGRAPHICAL ERRORS. WE DO NOT
+        WARRANT THAT THE CONTENT WILL BE UNINTERRUPTED OR ERROR FREE. WE DO NOT
+        WARRANT THAT Shill.lol SERVICES, INFORMATION, CONTENT, MATERIALS,
+        PRODUCTS (INCLUDING SOFTWARE) OR OTHER SERVICES INCLUDED ON OR OTHERWISE
+        MADE AVAILABLE TO YOU THROUGH Shill.lol SERVICES, SERVERS OR ELECTRONIC
+        COMMUNICATIONS SENT FROM Shill.lol ARE FREE OF VIRUSES OR OTHER HARMFUL
+        COMPONENTS. TO THE FULL EXTENT PERMISSIBLE BY LAW, Shill.lol WILL NOT BE
+        LIABLE FOR ANY DAMAGES OF ANY KIND ARISING FROM THE USE OF ANY Shill.lol
+        SERVICE, OR FROM ANY INFORMATION, CONTENT, MATERIALS, PRODUCTS
+        (INCLUDING SOFTWARE) OR OTHER SERVICES INCLUDED ON OR OTHERWISE MADE
+        AVAILABLE TO YOU THROUGH ANY Shill.lol SERVICE, INCLUDING, BUT NOT
+        LIMITED TO DIRECT, INDIRECT, INCIDENTAL, PUNITIVE, AND CONSEQUENTIAL
+        DAMAGES, UNLESS OTHERWISE SPECIFIED IN WRITING. TO THE MAXIMUM EXTENT
+        PERMITTED BY APPLICABLE LAW, OUR TOTAL LIABILITY TO YOU FOR ANY DAMAGES
+        (REGARDLESS OF THE FOUNDATION FOR THE ACTION) SHALL NOT EXCEED IN THE
+        AMOUNT YOU PAID US FOR THE APPLICABLE PRODUCT OR SERVICE. DISPUTES,
+        REMEDIES & APPLICABLE LAW Any dispute or claim relating in any way to
+        your use of any Shill.lol Service, or to any products or services sold
+        or distributed by Shill.lol or through shill.lol or any other Shill.lol
+        website will be resolved by binding arbitration, rather than in court.
+        You agree that the Federal Arbitration Act, applicable federal law, and
+        the laws of the state of Florida (without regard to principles of
+        conflict of laws), will govern these Terms of Service and Conditions of
+        Use and any dispute of any sort that might arise between you and
+        Shill.lol. There is no judge or jury in arbitration, and court review of
+        an arbitration award is limited. However, an arbitrator can award on an
+        individual basis the same damages and relief as a court (including
+        injunctive and declaratory relief or statutory damages), and must follow
+        the terms of these Terms of Service and Conditions of Use. Any
+        arbitration between us will be conducted by the American Arbitration
+        Association (AAA) under its rules, including the AAA's Supplementary
+        Procedures for Consumer-Related Disputes. The AAA's rules are available
+        at www.adr.org. The AAA's rules will govern payment of all filing,
+        administration and arbitrator fees. The arbitration will be conducted in
+        person in Miami-Dade County, Florida, at another mutually agreed-upon
+        location, or through such other procedures (such as, without limitation,
+        written submissions, phone or video attendance) as may be agreed by the
+        parties and approved by the arbitrator(s). You and Shill.lol both agree
+        that any dispute resolution proceedings will be conducted only on an
+        individual basis and not in a class, consolidated or representative
+        action. If for any reason a claim proceeds in court rather than in
+        arbitration you and Shill.lol each respectively waive any right to a
+        jury trial. You and Shill.lol also both agree that either of us may
+        bring suit in court to enjoin infringement or other misuse of
+        intellectual property rights.
+      </p>
+
+      <h2>MODIFICATION & SEVERABILITY</h2>
+
+      <p className="pb-5">
+        We reserve the right to make changes to our websites, policies, and
+        these Terms of Service and Conditions of Use at any time and without
+        notice to you. If any of these conditions shall be deemed invalid, void,
+        or for any reason unenforceable, that condition shall be deemed
+        severable and shall not affect the validity and enforceability of any
+        remaining condition.
+      </p>
+    </div>
+  );
+}
