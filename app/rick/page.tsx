c42032c34295d51a964742480cd4f98c1b07b0c4
Pancho Labrador <plabradorjr@gmail.com>
2023-05-21 21:48:29 -0500

    rick page

---
+++ app/rick/page.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "kek",
+  description: "you knew wat was coming",
+};
+
+export default function Rick() {
+  return (
+    <div className="video-responsive">
+      <iframe
+        width="853"
+        height="480"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ?&autoplay=1"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
+  );
+}
