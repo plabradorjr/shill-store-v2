efaf889f6ca6e09e7a5b64c0a8c5644296a8d3d1
Pancho Labrador <plabradorjr@gmail.com>
2023-05-29 11:48:28 -0500

    api request limit set to 100

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,7 +4,7 @@ import getProducts from "@/util/getProducts";
 export default async function Products() {
   const products = await getProducts();
   return (
-    <div className="flex flex-col-3 items-center justify-between gap-1 px-32">
+    <div className="grid grid-cols-4 items-center justify-between gap-1 px-32">
       {products.map((product) => (
         <Product {...product} key={product.id} />
       ))}
